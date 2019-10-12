62ef9356d8d04cc3e4abbea2f55f5b872c1926cb
Darek Li <darlicki@gmail.com>
2019-10-12 14:06:48 +0200

    ZgadnijLiczbe dziala

--- src/main/java/learning/CurrencyController.java
+++ src/main/java/learning/CurrencyController.java
@@ -13,14 +13,14 @@ public class CurrencyController {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        Rate forObject = restTemplate.getForObject("http://api.nbp.pl/api/exchangerates/rates/a/eur/2019-10-10/", Rate.class);
+       // Rate forObject = restTemplate.getForObject("http://api.nbp.pl/api/exchangerates/rates/a/eur/2019-10-10/", Rate.class);
 EuroRate forObject2 = restTemplate.getForObject("http://api.nbp.pl/api/exchangerates/rates/a/eur/2019-10-10/", EuroRate.class);
         //for (EuroRate euroRate : forObject) {
 
-           System.out.println(forObject.getMid());//todo null??
-        System.out.println(forObject2.getRates());
+          // System.out.println(forObject.getMid());//todo null??
+       // System.out.println(forObject2.getRates());
         double EurRate = forObject2.getRates().get(0).getMid();
-        System.out.println("Current EUR rate: "+ EurRate+ "PLN"); //todo is working
+        System.out.println("Current EUR rate: "+ EurRate+ " PLN"); //todo is working
 
 
         }
