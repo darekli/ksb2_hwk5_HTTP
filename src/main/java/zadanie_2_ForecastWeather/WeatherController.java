9f25fb15e59ce76b01449737753b448908260136
Darek Li <darlicki@gmail.com>
2019-10-16 16:00:25 +0200

    Http client ok random added zad 3 terminal version is working zad 1 Vaadin added. All done! zad 2 location ok, weather descr.&weather icon null

--- src/main/java/zadanie_2_ForecastWeather/WeatherController.java
+++ src/main/java/zadanie_2_ForecastWeather/WeatherController.java
@@ -16,18 +16,26 @@ public class WeatherController {
        // private void getShowWeather () {
             RestTemplate restTemplate = new RestTemplate();
 
-//            Weather forObject = restTemplate.getForObject("http://api.weatherstack.com/current?access_key=bee9823c5b680ef0e45834c2cc477727&query=Szczecin", Weather.class);
+       Weather forObjectWeather = restTemplate.getForObject("http://api.weatherstack.com/current?access_key=bee9823c5b680ef0e45834c2cc477727&query=Szczecin", Weather.class);
 //         //ExchangeRate forObject = restTemplate.getForObject("https://api.exchangeratesapi.io/latest", ExchangeRate.class);
 //           String location = forObject.getName();//todo its making error
-//         //  Location location2 = forObject.getLocation();
+    Location location2 = forObjectWeather.getLocation();
+        String locationName = forObjectWeather.getLocation().getName();
 //         //System.out.println("Current EUR rate: "+ EurRate+ " PLN"); //todo is working
 //            System.out.println(forObject + "/ " + location);
-           //System.out.println(location);
-          //  System.out.println(location2);
-
-           Current forObject2 = restTemplate.getForObject("http://api.weatherstack.com/current?access_key=bee9823c5b680ef0e45834c2cc477727&query=Szczecin", Current.class);
+        //   System.out.println(location);
+            System.out.println(location2);
+        System.out.println("Twoja wybrana lokalizacja: " +locationName);
+           Current forObjectCurrent = restTemplate.getForObject("http://api.weatherstack.com/current?access_key=bee9823c5b680ef0e45834c2cc477727&query=Szczecin", Current.class);
 //List<String> description = forObject2.getWeatherDescriptions();
-       //System.out.println(description);//todo null
+       List<String> weatherCondition = forObjectCurrent.getWeatherDescriptions();
+        List<String> weatherCondition2 = forObjectCurrent.getWeatherIcons();
+//double weatherCondition3 = forObjectCurrent.getTemperature();
+        System.out.println("Opis pogody "+weatherCondition);
+        System.out.println("Ikonka z pogodą " +weatherCondition2);
+     //   System.out.println("Temperatura "+ weatherCondition3);
+        //System.out.println(weatherCondition3);
+        //System.out.println(description);//todo null
 //            System.out.println(forObject2);
       //     List<String> sunny = forObject2.getWeatherDescriptions();//todo null
         //    System.out.println(sunny);
